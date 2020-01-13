b2062454cd11dc0fcd6dfb30843c0fc42671ab20
Ryan H <46948579+Le-SirH@users.noreply.github.com>
2020-01-13 10:15:25 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,18 @@
 
 # Encryption
-def run(input):
-    return [ord(input[i]) * key for i in range(len(input))]
+def run(input): return [ord(input[i]) * key for i in range(len(input))]
 
 def encrypt(fname):
-    f = open(fname, 'r')
-    output = run(f.read())
-    f.close()
-    with open(fname, 'w') as b:
-        for l in output: b.write('%s\n' % l)
+    with open (fname, 'r') as b: output = run(b.read())
+    with open(fname, 'w') as b: for l in output: b.write('%s\n' % l)
 
 def decrypt(fname):
-    with open(fname, 'r') as b:
-        encrypted = [l[:-1] for l in b]     
-    f = open(filename, 'w')
-    f.write(''.join([
-        chr(int(input[i])/int(key)) for i in range(len(encrypted))
-    ]))
-    f.close()
+    with open(fname, 'r') as b: encrypted = [l[:-1] for l in b]
+    with open(fname, 'w') as b: b.write(''.join([chr(int(input[i])/int(key)) for i in range(len(encrypted))]))
 
-path = str(input('File Path (relative): '))
-key = int(input('Encryption Key (integer): '))
+path, key = str(input('File Path (relative): ')), int(input('Encryption Key (integer): '))
 
-if int(input('\nEncrypt (1) or decrypt (2)?: ')) == 1: encrypt(path)
+if int(input('\nEncrypt (1) or decrypt (2)?: ')): encrypt(path)
 else: decrypt(path)
 
 print('\nDone\n')
