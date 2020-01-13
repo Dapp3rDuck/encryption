1c909cb698c5c558c379975eb360b7056a3268f4
Ryan H <46948579+Le-SirH@users.noreply.github.com>
2020-01-13 10:19:59 -0700

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,12 +1,6 @@
+# key is not defined
+def run(input): return [ord(input[x]*key for x in range(len(input))]
+
 def encrypt(filename):
-    def run(input):
-        output = []
-        for x in range (len(input)):
-            output.append(ord(input[x])*key)
-        return(output)
-    f = open(filename, 'r')
-    list = run(f.read())
-    f.close()
-    with open(filename, 'w') as b:
-        for listitem in list:
-            b.write('%s ' % listitem)
+    with open(filename, 'r') as f: list = run(f.read())
+    with open(filename, 'w') as b: for listitem in list: b.write('%s ' % listitem)
